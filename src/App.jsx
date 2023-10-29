5e5594f370af3e7d37fdc2f870522f5f7e29ac30
brown <86018533+2brownc@users.noreply.github.com>
2023-10-29 23:12:48 +0530

    refactor ui: home page

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,34 @@ import {
   Container,
   Title,
   Text,
+  Button,
+  Center,
+  Flex,
 } from '@mantine/core'
-import { Outlet } from 'react-router-dom';
+import { Outlet, useMatch, useNavigate } from 'react-router-dom';
 
 import { Header } from './components/Header/Header'
 
 function Intro() {
-  return <Container mb={100}>
-    <Title order={1}>Business Intel</Title>
-    <Text fs="italic">
-      Helps you gain a deeper understanding of the financial situation and make data-driven decisions to work towards financial goals with confidence.
-    </Text>
+  const navigate = useNavigate()
+  return <Container>
+    <Flex gap="xl" direction="column">
+      <Center>
+        <Title order={1}>Business Intel</Title>
+      </Center>
+      <Text fs="italic">
+        BusinessIntel is an analytics platform that provides in-depth insights and analysis for businesses and individuals. It helps users make informed financial decisions and optimize their financial strategies.
+      </Text>
+      <Center>
+        <Button variant="filled" onClick={() => navigate('/upload')}>Start!</Button>
+      </Center>
+    </Flex>
   </Container>
 }
 
 export default function App({ children }) {
+  const isHome = useMatch('/')
+
   return (
     <AppShell
       header={{ height: 60 }}
@@ -30,7 +43,7 @@ export default function App({ children }) {
 
       <AppShell.Main>
         <Container>
-          <Intro />
+          {isHome && <Intro />}
           <Outlet />
         </Container>
       </AppShell.Main>
