e353c6d911f4e9ea59361b49021187de7da1ebdb
brown <86018533+2brownc@users.noreply.github.com>
2023-10-29 23:19:28 +0530

    refactor ui: upload page

--- src/pages/Upload/Upload.jsx
+++ src/pages/Upload/Upload.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import {
   Container,
+  Title,
+  Text,
+  Center,
 } from '@mantine/core';
 
 import LoadData from '../../components/LoadData/LoadData'
 
 export default function Upload() {
-  return <>
-
-    <Container>
-      <LoadData />
-    </Container>
-
-  </>
+  return <Container>
+    <Title order={2} mb={15}>Upload Data</Title>
+    <Center>
+      <Text fs="italic" mb={10}>
+        Upload csv files containting transaction data to analyze. You can upload multiple files at once.
+      </Text>
+    </Center>
+    <LoadData />
+  </Container>
 }
